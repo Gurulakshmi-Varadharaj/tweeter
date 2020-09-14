b39469a39be18c2a355451c79e7953b62b496c96
Gurulakshmi Varadharaj <gurulakshmi.varadharaj@gmail.com>
2020-09-14 20:50:55 +0000

    Submit for evaluation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,38 +3,10 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 //Helper function to append newTweets 
 const createTweetElement = (newTweet) => {
-  console.log('Create Tweet is running');
   const $tweet = `
+  <article class = 'tweet'>
   <header>
     <img
       src='${newTweet['user']['avatars']}'>
@@ -45,9 +17,10 @@ const createTweetElement = (newTweet) => {
     '${newTweet['content']['text']}'
   </main>
   <footer>
-    <p>10 days ago</p>
+    <p>'${newTweet['created_at']}'</p>
     <p>Like</p>
   </footer>
+  </article>
   `
   return $tweet;
 }
@@ -56,11 +29,45 @@ const createTweetElement = (newTweet) => {
 const renderTweets = (tweets) => {
   for (let tweet of tweets) {
     let newTweet = createTweetElement(tweet);
-    $('.tweet').append(newTweet);
+    $('.addTweet').append(newTweet);
   }
 };
 
-$(document).ready(() => {
-  console.log('everything loaded');
-  renderTweets(data);
-})
+//Ajax and JQuery
+$(document).ready(function () {
+  //Helper function for ajax GET request to fetch tweets data from /tweets/
+  const loadTweets = (data) => {
+    $.ajax('http://localhost:8080/tweets', { method: 'GET', data: data })
+      .then((getJsonInput) => {
+        renderTweets(getJsonInput);
+      });
+  };
+  loadTweets();
+  //Submission of form using JQuery events
+  $('#submitTweet').on('submit', function (evt) {
+    //To stop the default behaviour of form element
+    evt.preventDefault();
+    //Handling validation errors for user input data
+    if ($('#tweet-text').val() === '') {
+      return $('.errorMsg').text('Please fill text to create new tweet');
+    } else if ($('.counter').val() <= 0) {
+      return $('.errorMsg').text('Too long. Please respect arbitary limit for 140 chars');
+    }
+    const userInput = $('#tweet-text').val();
+    $('#tweet-text').text(userInput);
+    //Serialize form data to get query stirng
+    let userText = $(this).serialize();
+    //ajax POST request for appending New Tweets
+    $.ajax({
+      url: "/tweets/",
+      method: "POST",
+      data: userText,
+      success: function (data) {
+        loadTweets(data);
+        $('#tweet-text').val("");
+        $('.counter').val('140');
+        return $('.errorMsg').text("");
+      },
+    });
+  });
+});
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  //Count input characters when tying text in textarea element and display in counter element
+  let count = $('.counter').val();
+  $('#tweet-text').on('keydown', function() {
+    let input = $('#tweet-text').val();
+    $('.counter').val(count - input.length);
+    //Change color if the number of input characters > 140
+    if ($('.counter').val() < 0) {
+      $('.counter').css("color", "#FF0000");
+    } else {
+      $('.counter').css("color", "#000000");
+    }
+  });
+
+  //Handling the Hover using JQuery mouseover event
+  $('.tweet').on('mouseover', function() {
+    $(this).find("span").val();
+  });
+  $('.tweet').on('mouseout', function() {
+    $(this).find("span").val("");
+  });
+});
